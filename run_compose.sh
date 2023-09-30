85c6014cd9e1d63c48222ca089e587f93ecf1115
Noelbiggs28 <noelbiggs28@gmail.com>
2023-09-30 08:03:52 -1000

    fixed container name

--- run_compose.sh
+++ run_compose.sh
@@ -2,5 +2,5 @@ docker-compose up -d --build
 
 # make sure the postgres container is ready, then run migrations
 sleep 5
-docker exec dog_park_django-api-1 python /src/manage.py makemigrations 
-docker exec dog_park_django-api-1 python /src/manage.py migrate
+docker exec dog_park_api-1 python /src/manage.py makemigrations 
+docker exec dog_park_api-1 python /src/manage.py migrate
