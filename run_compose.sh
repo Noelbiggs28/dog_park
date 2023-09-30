c07b379048bc7e9ebb5497dce82841f8241052fd
Noelbiggs28 <noelbiggs28@gmail.com>
2023-09-30 09:47:09 -1000

    eh

--- run_compose.sh
+++ run_compose.sh
@@ -4,6 +4,6 @@
 # sleep 5
 # docker exec dog_park-api-1 python /src/manage.py makemigrations 
 # docker exec dog_park-api-1 python /src/manage.py migrate
-COMPOSE_DOCKER_CLI_BUILD=0 DOCKER_BUILDKIT=0 docker-compose up -d
+docker-compose up -d --build
 sleep 10
 docker exec dog_park-api-1 python manage.py migrate
