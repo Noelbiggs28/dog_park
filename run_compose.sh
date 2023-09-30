7435a91bd12ec21cf5bb1e094b8308c7f0ab881a
Noelbiggs28 <noelbiggs28@gmail.com>
2023-09-30 08:32:33 -1000

    fixed hyphen

--- run_compose.sh
+++ run_compose.sh
@@ -6,4 +6,4 @@
 # docker exec dog_park-api-1 python /src/manage.py migrate
 COMPOSE_DOCKER_CLI_BUILD=0 DOCKER_BUILDKIT=0 docker-compose up -d
 sleep 10
-docker exec bookstore-api-1 python manage.py migrate
+docker exec dog_park-api-1 python manage.py migrate
